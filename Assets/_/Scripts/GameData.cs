c254f742c476d0951b23f0f9d8491f4e55a3bf17
Weshmangle <96111319+Weshmangle@users.noreply.github.com>
2025-04-26 17:11:23 +0200

    Update menu create GameData into BGS / GameData

--- Assets/_/Scripts/GameData.cs
+++ Assets/_/Scripts/GameData.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "GameData", menuName = "GameData", order = 0)]
+[CreateAssetMenu(fileName = "GameData", menuName = "BGS/GameData", order = 0)]
 public class GameData : ScriptableObject
 {
     
